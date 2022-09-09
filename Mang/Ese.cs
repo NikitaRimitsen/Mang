f8aec6767ce56b72b0f76094a79f44b575c740ab
opilane <opilane@HP-CZC6297B9B>
2022-09-09 11:38:00 +0300

    Add project files.

---
+++ Mang/Ese.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mang
+{
+    class Ese : Entity
+    {
+        public string nimi;
+        public int arv;
+
+        public Ese(string nimi, int punktidearv)
+        {
+            this.nimi = nimi;
+            this.arv = punktidearv;
+        }
+
+        public string info() 
+        { 
+            return nimi; 
+        }
+        public int punktidearv()
+        {
+            return arv;
+        }
+    }
+}
---
+++ Mang/Tegelane.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mang
+{
+    class Tegelane : Entity
+    {
+        public string nimi;
+        List<Ese> ese = new List<Ese>();
+
+        public Tegelane(string nimi)
+        {
+            List<Ese> ese;
+            this.nimi=nimi;
+        }
+        public void lisaEse()
+        {
+
+        }
+        public void Info()
+        {
+
+        }
+        public void punktideArv()
+        {
+
+        }
+        public void valjastaEsemed()
+        {
+
+        }
+    }
+}
